f57bf68e9ba6debc875f80861a7f122ed58f08cf
Lukainit <santanazar123@icloud.com>
2024-01-18 19:03:01 +0200

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,115 @@
-/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+if (navClose) {
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu');
+    });
+ }
 
+const navLinks = document.querySelectorAll(".nav__link");
 
-/*=============== SHADOW HEADER ===============*/
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+};
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', linkAction);
+});
+
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    window.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow -header');
+}
+
+window.addEventListener('scroll', shadowHeader); 
+  
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+    const sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']');
+
+    if (scrollDown >= sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
 
 
 /*=============== EMAIL JS ===============*/
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    window.scrollY >= 350
+      ? scrollUp.classList.add('show-scroll')
+      : scrollUp.classList.remove('show-scroll');
+  };
+  
+window.addEventListener('scroll', scrollUp);
 
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+const sr = ScrollReveal();
 
+sr.reveal('.home_perfil', {
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+sr.reveal('.home__name, .home__info, .about__image, .home__perfil, about__container .section__title-1, about__info, .section__title-1, .about__info, .contact_social, .contactl ', {
+  origin: 'left',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
